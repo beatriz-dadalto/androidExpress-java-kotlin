7a679e03c2ed3d077451a6c82ae3ace9d7f777a9
Bia Coelho <biacoelho@pm.me>
2021-05-02 00:57:46 -0300

    add: aula Colecoes de Views Reciclaveis

--- 7-formularios-colecoes-estilos/FitnessTracker/app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
+++ 7-formularios-colecoes-estilos/FitnessTracker/app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
@@ -1,28 +1,63 @@
 package co.tiagoaguiar.codelab.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
-	private View btnImc;
+	private RecyclerView rvMain;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		btnImc = findViewById(R.id.btn_imc);
+		rvMain = findViewById(R.id.main_rv);
 
-		// obj interno que será executado quando eu clicar no botão
-		btnImc.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, ImcActivity.class);
-				startActivity(intent);
-			}
-		});
+		// Primeiro: definir o comportamento de exibicao do layout da recyclerView
+			// ela pode ser do tipo: mosaic, grid ou linear (horizontal ou vertical)
+		rvMain.setLayoutManager(new LinearLayoutManager(this));
+
+		MainAdapter adapter = new MainAdapter();
+		rvMain.setAdapter(adapter);
+
+	}
+
+	private class MainAdapter extends RecyclerView.Adapter<MainViewHolder> {
+
+		@NonNull
+		@Override
+		public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+			return new MainViewHolder(getLayoutInflater().inflate(R.layout.main_item, parent, false));
+		}
+
+		@Override
+		public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+			holder.bind(position);
+		}
+
+		@Override
+		public int getItemCount() {
+			return 15;
+		}
+	}
+
+	// eh a view do item que aparece no recyclerView
+	private class MainViewHolder extends RecyclerView.ViewHolder {
+
+		public MainViewHolder(@NonNull View itemView) {
+			super(itemView);
+		}
+
+		public void bind(int position) {
+			TextView textTest = itemView.findViewById(R.id.textView_test);
+			textTest.setText("Teste de rolagem " + position);
+		}
 	}
 }
