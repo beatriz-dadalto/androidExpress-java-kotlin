d9f347e001c862c6748cbc299dfdb890df253809
Bia Coelho <biacoelho@pm.me>
2021-05-06 23:19:46 -0300

    add: aula Tela Listagem dos resultados

--- 7-formularios-colecoes-estilos/FitnessTracker/app/src/main/java/co/tiagoaguiar/codelab/myapplication/ListCalcActivity.java
+++ 7-formularios-colecoes-estilos/FitnessTracker/app/src/main/java/co/tiagoaguiar/codelab/myapplication/ListCalcActivity.java
@@ -1,11 +1,23 @@
 package co.tiagoaguiar.codelab.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class ListCalcActivity extends AppCompatActivity {
 
@@ -16,6 +28,8 @@ public class ListCalcActivity extends AppCompatActivity {
 
     Bundle extras = getIntent().getExtras();
 
+    RecyclerView recyclerView = findViewById(R.id.recycler_view_list);
+
     if (extras != null) {
       String type =  extras.getString("type");
 
@@ -24,8 +38,62 @@ public class ListCalcActivity extends AppCompatActivity {
 
         runOnUiThread(() -> {
           Log.d("Teste", registers.toString());
+          ListCalcAdapter adapter = new ListCalcAdapter(registers);
+          recyclerView.setLayoutManager(new LinearLayoutManager(this));
+          recyclerView.setAdapter(adapter);
         });
       }).start();
+    }
+  }
+
+  private class ListCalcAdapter extends RecyclerView.Adapter<ListCalcAdapter.ListCalcViewHolder> {
+
+    private List<Register> datas;
+    private OnItemClickListener listener;
+
+    public ListCalcAdapter(List<Register> datas) {
+      this.datas = datas;
+    }
+
+    @NonNull
+    @Override
+    public ListCalcViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+      return new ListCalcViewHolder(getLayoutInflater().inflate(android.R.layout.simple_list_item_1, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListCalcViewHolder holder, int position) {
+      Register data = datas.get(position);
+      holder.bind(data);
+    }
+
+    @Override
+    public int getItemCount() {
+      return datas.size();
+    }
+
+    // eh a view do item que aparece no recyclerView
+    private class ListCalcViewHolder extends RecyclerView.ViewHolder {
+
+      public ListCalcViewHolder(@NonNull View itemView) {
+        super(itemView);
+      }
+
+      public void bind(Register data) {
+        String formatted = "";
+        try {
+          SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", new Locale("pt", "BR"));
+          Date dateSaved = sdf.parse(data.createdDate);
+          SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", new Locale("pt", "BR"));
+          formatted = dateFormat.format(dateSaved);
+        } catch (ParseException e) {
+          Log.e("Erro", e.getMessage());
+        }
+
+        ((TextView) itemView).setText(
+            getString(R.string.list_response, data.response, formatted)
+        );
+      }
     }
   }
 }
