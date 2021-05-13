4018286c65445f04c0b997f0f3b4da89ae216415
Bia Coelho <biacoelho@pm.me>
2021-05-13 04:15:16 -0300

    Capa do filme com sombras

--- 9-netflix-detalhes-e-filmes-similares/app/src/main/java/com/bcoelho/netflixremake/MovieActivity.java
+++ 9-netflix-detalhes-e-filmes-similares/app/src/main/java/com/bcoelho/netflixremake/MovieActivity.java
@@ -2,8 +2,12 @@ package com.bcoelho.netflixremake;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -18,6 +22,13 @@ public class MovieActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
+        }
+
+        LayerDrawable drawable = (LayerDrawable) ContextCompat.getDrawable(this, R.drawable.shadows);
+        if (drawable != null) {
+            Drawable movieCover = ContextCompat.getDrawable(this, R.drawable.movie);
+            drawable.setDrawableByLayerId(R.id.cover_drawable, movieCover);
+            ((ImageView) findViewById(R.id.image_view_cover)).setImageDrawable(drawable);
         }
     }
 }
