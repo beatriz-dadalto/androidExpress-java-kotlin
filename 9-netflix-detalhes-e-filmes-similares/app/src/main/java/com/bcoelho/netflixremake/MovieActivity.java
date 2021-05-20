7428c3fc9cc6f01ce3f417de6b6423a75e915e9a
Bia Coelho <biacoelho@pm.me>
2021-05-20 00:36:56 -0300

    ScrollView e RecyclerView

--- 9-netflix-detalhes-e-filmes-similares/app/src/main/java/com/bcoelho/netflixremake/MovieActivity.java
+++ 9-netflix-detalhes-e-filmes-similares/app/src/main/java/com/bcoelho/netflixremake/MovieActivity.java
@@ -1,20 +1,31 @@
 package com.bcoelho.netflixremake;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bcoelho.netflixremake.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MovieActivity extends AppCompatActivity {
 
     private TextView txtTitle;
     private TextView txtDesc;
     private TextView txtCast;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,7 @@ public class MovieActivity extends AppCompatActivity {
         txtTitle = findViewById(R.id.text_view_title);
         txtDesc = findViewById(R.id.text_view_desc);
         txtCast = findViewById(R.id.text_view_cast);
+        recyclerView = findViewById(R.id.recycler_view_similar);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -44,5 +56,50 @@ public class MovieActivity extends AppCompatActivity {
         txtTitle.setText("Batman Begins");
         txtDesc.setText("sdfkjshdfjkashdfksdihguhsdlfuhgdlkjhfglsdjfglsdjfghsdkfjgslhkdfjghsdk");
         txtCast.setText(getString(R.string.cast, "Cristian Bale" + ", Michael Caine" + ", Liam Nesche "));
+
+        List<Movie> movies  = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            Movie movie = new Movie();
+            movies.add(movie);
+        }
+
+        recyclerView.setAdapter(new MovieAdapter(movies));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+    }
+
+    private static class MovieHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView imageViewCover;
+
+        public MovieHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewCover = itemView.findViewById(R.id.image_view_cover);
+        }
+    }
+
+    private class MovieAdapter extends RecyclerView.Adapter<MovieHolder> {
+
+        private final List<Movie> movies;
+
+        public MovieAdapter(List<Movie> movies) {
+            this.movies = movies;
+        }
+
+        @NonNull
+        @Override
+        public MovieHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MovieHolder(getLayoutInflater().inflate(R.layout.movie_item_similar, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MovieHolder holder, int position) {
+            Movie movie = movies.get(position);
+//      holder.imageViewCover.setImageResource(movie.getCoverUrl());
+        }
+
+        @Override
+        public int getItemCount() {
+            return movies.size();
+        }
     }
 }
